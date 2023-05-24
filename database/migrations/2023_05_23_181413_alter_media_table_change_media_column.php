4c224c7786d29de996795b83dd8ce0ca21dfac6b
Ophélien DUPARC <ophelien.duparc@projetretro.io>
2023-05-24 22:49:03 +0200

    aalal

--- database/migrations/2023_05_23_181413_alter_media_table_change_media_column.php
+++ database/migrations/2023_05_23_181413_alter_media_table_change_media_column.php
@@ -17,6 +17,7 @@ return new class extends Migration
             //
             $table->dropIndex(['media', 'destination']);
             $table->dropUnique(['destination']);
+            $table->dropUnique(['media']);
       //
         });
     }
