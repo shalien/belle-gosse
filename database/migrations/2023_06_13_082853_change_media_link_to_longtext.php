707bd1f555c3da1afdcd1c7e98ae6b67ad19d7c4
Ophélien DUPARC <ophelien.duparc@projetretro.io>
2023-06-13 10:30:21 +0200

    Renewed table

---
+++ database/migrations/2023_06_13_082853_change_media_link_to_longtext.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('medias', function (Blueprint $table) {
+            //
+
+            $table->dropUnique('medias_link_unique');
+
+            $table->longText('link')->change();
+
+            $table->unique('link');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('medias', function (Blueprint $table) {
+            //
+        });
+    }
+};
