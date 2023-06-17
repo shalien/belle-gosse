0cd84bf3b3a14f58dd8b450337f713c679f601fb
unknown <ophelien.duparc@projetretro.io>
2023-06-17 21:31:30 +0200

    Added not null to FK

---
+++ database/migrations/2023_06_17_192538_set_not_nullable_medias.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('medias', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('source_id')->nullable(false)->change();
+            $table->unsignedBigInteger('destination_id')->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('medias', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ database/migrations/2023_06_17_192929_set_not_nullable_providers.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('providers', function (Blueprint $table) {
+            //
+
+            $table->unsignedBigInteger('topic_id')->nullable(false)->change();
+            $table->unsignedBigInteger('provider_type_id')->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('providers', function (Blueprint $table) {
+            //
+        });
+    }
+};
